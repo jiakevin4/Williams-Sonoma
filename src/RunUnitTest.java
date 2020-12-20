1250bdeb9cad0f114b404c9849906b907100100f
Your Name <you@example.com>
2020-12-20 02:45:37 -0500

    Coding Challenge

--- src/RunUnitTest.java
+++ src/RunUnitTest.java
@@ -26,23 +26,23 @@ public class RunUnitTest {
 		
 		//Write the result of Unit Tests, and print any failed cases
 		try {
-        	//Write time stamp
-        	FileWriter writeLog = new FileWriter("Inputs And Outputs.txt", true);
-        	Date currentDateTime = new Date();
-        	writeLog.write("Produced the minimum number of ranges at: " + currentDateTime.toString() + "\r\n" + "\r\n");
+			//Write time stamp
+			FileWriter writeLog = new FileWriter("Inputs And Outputs.txt", true);
+			Date currentDateTime = new Date();
+			writeLog.write("Produced the minimum number of ranges at: " + currentDateTime.toString() + "\r\n" + "\r\n");
         	
-        	//Write and print any failed cases
-        	for(Failure fail: unitTestResult.getFailures()) {
-    			System.out.println("Failed Test Case: " + fail.toString());
-    			writeLog.write("Failed Test Case: " + fail.toString() + "\r\n");
-    		}
+			//Write and print any failed cases
+			for(Failure fail: unitTestResult.getFailures()) {
+				System.out.println("Failed Test Case: " + fail.toString());
+				writeLog.write("Failed Test Case: " + fail.toString() + "\r\n");
+			}
     		
         	//Write and print result of Unit Test
-    		System.out.println("All Test Cases Passed: " + unitTestResult.wasSuccessful());
-    		writeLog.write("All Test Cases Passed: " + unitTestResult.wasSuccessful() + "\r\n");
-    		writeLog.write("\r\n\r\n");
-        	writeLog.close();
-        	}
+			System.out.println("All Test Cases Passed: " + unitTestResult.wasSuccessful());
+			writeLog.write("All Test Cases Passed: " + unitTestResult.wasSuccessful() + "\r\n");
+			writeLog.write("\r\n\r\n");
+			writeLog.close();
+        }
 		catch (IOException e) {
         	e.printStackTrace();
         }
